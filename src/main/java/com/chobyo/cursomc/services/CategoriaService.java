700f1dd1cb2bdb1f5d8be44e7f67cd36e47c0bfd
chobyo <github@chobyo.com>
2020-06-03 01:29:17 -0300

    Paginação com parâmetros opcionais

--- src/main/java/com/chobyo/cursomc/services/CategoriaService.java
+++ src/main/java/com/chobyo/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.chobyo.cursomc.domains.Categoria;
@@ -45,6 +48,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
-		
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction)
+	{
+		PageRequest pageReques = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageReques);
 	}
 }
